0544616987e0a2dbbe2426cc32171487071eae18
dfrojas20 <123292688+dfrojas20@users.noreply.github.com>
2024-05-17 20:42:41 -0400

    agregada mecanica de disparo (arreglar animacion)

--- Assets/Script/Player/playerAttack.cs
+++ Assets/Script/Player/playerAttack.cs
@@ -6,6 +6,8 @@ public class playerAttack : MonoBehaviour
     private Animator anim;
     private PlayerController playerMovement;
     private float cooldownTimer = Mathf.Infinity;
+    [SerializeField]  private Transform bulletPoint;
+    [SerializeField]  private GameObject[] bullets;  
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +30,11 @@ public class playerAttack : MonoBehaviour
     {
         anim.SetTrigger("attack");
         cooldownTimer = 0;
+        //pool bullets
+
+        bullets[0].transform.position = bulletPoint.position;
+        bullets[0].GetComponent<proyectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+
     }
 
 }
